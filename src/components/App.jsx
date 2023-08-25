6c154c0f30b9f3ec6ec40ef3401b0b0fb363f15c
Abhay Singh <64921353+deltaTH@users.noreply.github.com>
2023-08-25 10:37:42 +0530

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,33 @@
+//create entry component - done
+//create props to replace hardcoaded data - done
+//import the emojipedia constant - done
+//map through the emojipedia array and render entry component - done
+
+import React from "react";
+import Entry from "./Entry";
+import emojipedia from "../emojipedia";
+
+function createEntry(emojiTerm) {
+  return (
+    <Entry
+      key={emojiTerm.id}
+      emoji={emojiTerm.emoji}
+      name={emojiTerm.name}
+      desc={emojiTerm.meaning}
+    />
+  );
+}
+
+function App() {
+  return (
+    <div>
+      <h1>
+        <span>emojipedia</span>
+      </h1>
+
+      <dl className="dictionary">{emojipedia.map(createEntry)}</dl>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/emojipedia.js
@@ -0,0 +1,142 @@
+const emojipedia = [
+  {
+    id: 1,
+    emoji: "💪",
+    name: "Tense Biceps",
+    meaning:
+      "“You can do that!” or “I feel strong!” Arm with tense biceps. Also used in connection with doing sports, e.g. at the gym."
+  },
+  {
+    id: 2,
+    emoji: "🙏",
+    name: "Person With Folded Hands",
+    meaning:
+      "Two hands pressed together. Is currently very introverted, saying a prayer, or hoping for enlightenment. Is also used as a “high five” or to say thank you."
+  },
+  {
+    id: 3,
+    emoji: "🤣",
+    name: "Rolling On The Floor, Laughing",
+    meaning:
+      "This is funny! A smiley face, rolling on the floor, laughing. The face is laughing boundlessly. The emoji version of “rofl“. Stands for „rolling on the floor, laughing“."
+  },
+
+  {
+    id: 4,
+    emoji: "🤞",
+    name: "Hand With Crossed Fingers",
+    meaning:
+      "The closed fist with crossed index and middle fingers is used to wish someone luck. Children usually use this gesture to annihilate a promise or oath."
+  },
+
+  {
+    id: 5,
+    emoji: "🙈",
+    name: "See-No-Evil Monkey",
+    meaning:
+      "Monkey with two hands in front of its eyes. Closes its eyes to the world so as not to see anything bad or cruel, or is embarrassed. Mizaru is one of the three wise monkeys. In a sense, ignoring bad things."
+  },
+
+  {
+    id: 6,
+    emoji: "🐱",
+    name: "Cat face",
+    meaning:
+      "Adorable illustration of cat face 🐱 shows a playful, fluffy velvet paw, which is a popular pet especially in big cities. Typically, the cat emoji is depicted as a yellow-orange cat with pointed ears and whiskers. The cute cats are known for their independence, stubbornness, and sixth sense. Symbolically, they represent wisdom, happiness, individuality, and tenderness."
+  },
+
+  {
+    id: 7,
+    emoji: "🍆",
+    name: "Eggplant",
+    meaning:
+      "The eggplant is associated with the male sex. It is one of the sex symbols. Is hated and loved on Instagram #freetheeggplant."
+  },
+
+  {
+    id: 8,
+    emoji: "❤️",
+    name: "Red Heart",
+    meaning:
+      "Love and affection are expressed by the red heart emoji ❤️. In romantic relationships, it represents passion and deep feelings. But it can also express friendship or family affection. In everyday life, it is often used to express gratitude or appreciation."
+  },
+  {
+    id: 9,
+    emoji: "🧡",
+    name: "Orange Heart",
+    meaning:
+      "An orange heart 🧡 is a versatile emoji that can be used in a variety of contexts. It represents both tender love and close friendship without conveying the same passion as the classic red heart. The orange heart can be used to express warmth and caring in a less romantic way, such as between siblings or close friends."
+  },
+  {
+    id: 10,
+    emoji: "🖤",
+    name: "Black Heart",
+    meaning:
+      "The black heart shows a dark and twisted sense of humor. It can reflect the dark side of the soul. Also used to express grief and sorrow."
+  },
+
+  {
+    id: 11,
+    emoji: "🤍",
+    name: "White heart",
+    meaning:
+      "The heart is an international symbol of love, affection and compassion. A white heart can symbolize the purity, kindness and innocence of a relationship or friendship."
+  },
+
+  {
+    id: 12,
+    emoji: "💔",
+    name: "Broken Heart",
+    meaning:
+      "I am desperately unhappy! The broken red heart expresses sadness and suffering. Someone is love sick at the moment."
+  },
+
+  {
+    id: 13,
+    emoji: "🥹",
+    name: "Face Holding Back Tears",
+    meaning:
+      "The 🥹 emoji, which shows a slightly smiling face with tears in its eyes, can express various emotions such as gratitude, emotion, or pride. At the same time, anger or rage can also be the reason why tears can hardly be held back. It is often used to express strong feelings in communication, and the emotional interpretation depends on the context."
+  },
+
+  {
+    id: 14,
+    emoji: "😅",
+    name: "Smiling Face With Open Mouth & Cold Sweat",
+    meaning:
+      " grinning face with drops of sweat is represented by the 😅 emoji. This is used to express relief that a tricky situation has been successfully handled or just gone well, such as an important meeting. It is also sometimes used to express nervousness or tension about upcoming difficult events."
+  },
+
+  {
+    id: 15,
+    emoji: "😂",
+    name: "Face With Tears of Joy",
+    meaning:
+      "The 😂 emoji, which represents a laughing face with tears of joy, expresses that you can barely hold yourself on the chair with laughter. It symbolizes a situation where something is so incredibly funny that you are in tears and in a fit of laughter. Often used to highlight funny or amusing moments where you can barely hold yourself up with laughter. "
+  },
+
+  {
+    id: 16,
+    emoji: "🥲",
+    name: "Smiling face with tear",
+    meaning:
+      "The 🥲 emoji stands for smiling through the tough times and symbolizes trying to stay positive even in difficult moments. You are smiling through the pain instead of being truly happy. It shows the ability to hide true feelings behind a smile when dealing with problems or difficult times in life. "
+  },
+
+  {
+    id: 17,
+    emoji: "∆+#",
+    name: "deltaTH",
+    meaning: "Made with ❤️ by Abhay "
+  },
+
+  {
+    id: 18,
+    emoji: "🙃",
+    name: "Upside-Down Face",
+    meaning:
+      "I'm not serious! The inverted smiling face 🙃 indicates that you are not serious or have a humorous undertone. It is suitable for ambiguous statements, ironic remarks or joking comments. Also, it can be used to express silliness or in response to a joke. In conversations, it emphasizes the playful nature of a message and helps clarify potential misunderstandings about the tone of the message."
+  }
+];
+
+export default emojipedia;
